9ef8d29958b902f9a33e8c9f18c81ee004df3ff7
camber boles <camberb51@gmail.com>
2023-06-19 11:59:34 -0400

    timeout and change aria-expanded

--- js/navmenu.js
+++ js/navmenu.js
@@ -1,17 +1,23 @@
 // Queries the submenu(s)
 var itemsWithSubmenu = $('li.has-submenu');
+var timeDelay;
 
 // Delays dropdown menu for mouse users
 // Adds class 'open' on mouseover,
 itemsWithSubmenu.on('mouseover', function() {
     $(this).addClass('open');
+    clearTimeout(timeDelay);
 });
 
 // and then removes class 'open' 1 second
 // after mouseout.
 itemsWithSubmenu.on('mouseout', function() {
-    $(this).delay(1000).removeClass('open');
+    timeDelay = setTimeout(closeMenu, 1000, $(this));
 });
+
+function closeMenu(jqObject) {
+    jqObject.removeClass('open');
+}
 
 // Allows on-click toggle for keyboard users
 // If the menu is already open on click,
@@ -20,10 +26,13 @@ itemsWithSubmenu.on('mouseout', function() {
 itemsWithSubmenu.on('click', function(e) {
     e.preventDefault();
     var menuToggle = $(this);
+    var toggleLink = $('a.submenu-toggle');
 
     if (menuToggle.attr('class').includes('open')) {
         menuToggle.removeClass('open');
+        toggleLink.attr('aria-expanded', 'true');
     } else {
         menuToggle.addClass('open');
+        toggleLink.attr('aria-expanded', 'false');
     }
 });
